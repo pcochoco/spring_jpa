65a9da5141db0590db1dac0a92cdecf32bd0d6af
pcochoco <cochocolatecoo@gmail.com>
2024-04-03 13:14:52 +0900

    item service, repository added

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/repository/ItemRepository.java
@@ -0,0 +1,35 @@
+package jpabook.jpashop.domain.repository;
+
+import jakarta.persistence.EntityManager;
+import jpabook.jpashop.domain.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    public void save(Item item){
+        if (item.getId() == null) {
+            em.persist(item); //item id 없는 경우 새로운 객체 생성한 것
+        } else {
+           em.merge(item);  //db에 등록된 경우 save는 update와 유사히 동작하는 것
+        }
+    }
+
+    public Item findOne(Long id){
+        return em.find(Item.class, id);
+    }
+
+    public List<Item> findAll(){
+        return em.createQuery("select i form Item i", Item.class)
+                .getResultList();
+    }
+
+
+
+}
