9cccccf8dba1f095ba9ef8ddad5d3ec097af28b4
pcochoco <cochocolatecoo@gmail.com>
2024-04-22 15:33:52 +0900

    order cancel for ordercontroller

--- jpashop/src/main/java/jpabook/jpashop/controller/OrderController.java
+++ jpashop/src/main/java/jpabook/jpashop/controller/OrderController.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -39,6 +40,11 @@ public class OrderController {
                         @RequestParam("count") int count){
         orderService.order(memberId, itemId, count);
         return "redirect:/orders";
+    }
+
+    @PostMapping(value = "/orders/{orderId}/cancel")
+    public String cancelOrder(@PathVariable("orderId") Long orderId){
+        orderService.cancelOrder(orderId);
     }
 
 }
